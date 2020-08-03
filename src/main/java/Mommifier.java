ab6596f56725b52fcb953d58f3315a645b1494e3
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-03 15:14:55 +0800

    [jianglin wang] feat: should return original when convert given vowels are not more than 30 precent

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Mommifier {
+
+    public static final ArrayList<Character> VOWELS = new ArrayList<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
     String convert(String string) {
-        return "";
+        if(string.equals("")) {
+            return "";
+        }
+        int vowelsCount = 0;
+        for (char item : VOWELS) {
+            if(VOWELS.contains(item)) {
+                vowelsCount ++;
+            }
+        }
+        if(vowelsCount / string.length() < 0.3) {
+            return string;
+        }
+        return null;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -8,5 +8,11 @@ public class MommifierTest {
         String result = mommifier.convert("");
         Assertions.assertEquals("", result);
     }
+    @Test
+    void should_return_original_when_convert_given_the_number_of_vowels_are_not_more_than_30_percent_of_the_string_length() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert("allset");
+        Assertions.assertEquals("allset", result);
+    }
 
 }
