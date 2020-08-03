3f5511bba19aba3af17ce65e405a68a4e7e6b89f
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-03 13:04:11 +0800

    [jianglin wang] feat: should return empty when convert given params is empty

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,5 @@
+public class Mommifier {
+    String convert(String string) {
+        return "";
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MommifierTest {
+    @Test
+    void should_return_empty_when_convert_given_params_is_empty() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert("");
+        Assertions.assertEquals("", result);
+    }
+
 }
