0e11f7a13b60fc8d6e971c0bf111fcab47fe6ca5
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-03 16:57:14 +0800

    [jianglin wang] feat: should throw error when convert given wrong params

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -6,9 +6,10 @@ public class Mommifier {
     public static final ArrayList<Character> VOWELS = new ArrayList<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
     public static final double LIMIT = 0.3;
 
-    String convert(String string) {
-        if(string.equals("")) {
-            return "";
+    String convert(String string) throws Exception{
+        if(string.equals("")) return "";
+        if(!string.matches("^[A-Za-z0-9]+$")) {
+            throw new Exception("input is wrong");
         }
         int vowelsCount = getVowelsCount();
         if(vowelLimitReached(string, vowelsCount)) {
@@ -23,7 +24,7 @@ public class Mommifier {
             }
             StringBuffer temp = new StringBuffer(string);
             for (int i = vowelIndexArr.size() - 1; i >= 0; i --) {
-                temp.insert((int)vowelIndexArr.get(i), "memmy");
+                temp.insert((int)vowelIndexArr.get(i), "mommy");
             }
             return temp.toString();
         }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -3,21 +3,32 @@ import org.junit.jupiter.api.Test;
 
 public class MommifierTest {
     @Test
-    void should_return_empty_when_convert_given_params_is_empty() {
+    void should_return_empty_when_convert_given_params_is_empty() throws Exception {
         Mommifier mommifier = new Mommifier();
         String result = mommifier.convert("");
         Assertions.assertEquals("", result);
     }
     @Test
-    void should_return_original_when_convert_given_the_number_of_vowels_are_less_than_30_percent_of_the_string_length() {
+    void should_return_original_when_convert_given_the_number_of_vowels_are_less_than_30_percent_of_the_string_length() throws Exception {
         Mommifier mommifier = new Mommifier();
         String result = mommifier.convert("allset");
         Assertions.assertEquals("allset", result);
     }
     @Test
-    void should_insert_mommy_for_each_continuous_set_of_vowels_when_convert_given_the_number_of_vowels_are_more_than_30_percent_of_the_string_length() {
+    void should_insert_mommy_for_each_continuous_set_of_vowels_when_convert_given_the_number_of_vowels_are_more_than_30_percent_of_the_string_length() throws Exception {
         Mommifier mommifier = new Mommifier();
         String result = mommifier.convert("aeuadde");
-//        Assertions.assertEquals("amommyemommyumommyadde", result);
+        Assertions.assertEquals("amommyemommyumommyadde", result);
+    }
+    @Test
+    void should_throw_error_when_convert_given_wrong_params() throws Exception {
+        Mommifier mommifier = new Mommifier();
+        Exception result = null;
+        try {
+            mommifier.convert("王");
+        }catch (Exception e) {
+            result = e;
+        }
+        Assertions.assertEquals("input is wrong", result.getMessage());
     }
 }
